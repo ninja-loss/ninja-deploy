78fda19a562a166790b066d38fc1bceb2ab5205b
Jason Harrelson <jason@lookforwardenterprises.com>
2010-11-10 00:14:55 -0600

    Add desc to log:tail recipe.

--- lib/ninja_deploy/recipes/log.rb
+++ lib/ninja_deploy/recipes/log.rb
@@ -1,5 +1,6 @@
 Capistrano::Configuration.instance( :must_exist ).load do
   namespace :log do
+    desc "Tail a log on the remote server"
     task :tail, :roles => :app do
       invoke_command "tail -f #{shared_path}/log/#{rails_env}.log"
     end
