20ead9b2f3be01d1527787caf0d939c489241b69
Jason Harrelson <jason@lookforwardenterprises.com>
2010-11-10 09:50:13 -0600

    Add passenger sudo restart recipe.

--- lib/ninja_deploy/recipes/passenger.rb
+++ lib/ninja_deploy/recipes/passenger.rb
@@ -2,6 +2,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :passenger do
     desc "Restart the passenger module to reload the application after deploying."
     task :restart, :roles => :app, :except => {:no_release => true} do
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+
+    desc "Restart the passenger module to reload the application after deploying using sudo."
+    task :sudo_restart, :roles => :app, :except => {:no_release => true} do
       sudo "touch #{current_path}/tmp/restart.txt"
     end
   end
