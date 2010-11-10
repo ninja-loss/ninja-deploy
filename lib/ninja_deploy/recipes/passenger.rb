87c6203526eb371d5f8d51ad11c4852b41779f9d
Jason Harrelson <jason@lookforwardenterprises.com>
2010-11-09 23:32:23 -0600

    Add passenger recipe.

---
+++ lib/ninja_deploy/recipes/passenger.rb
@@ -0,0 +1,8 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :passenger do
+    desc "Restart the passenger module to reload the application after deploying."
+    task :restart, :roles => :app, :except => {:no_release => true} do
+      sudo "touch #{current_path}/tmp/restart.txt"
+    end
+  end
+end
