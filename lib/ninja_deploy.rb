a1c04419937165313a72eba1598911f52fac1a73
Nils Jonsson <nils@alumni.rice.edu>
2012-03-14 16:17:13 -0500

    Expose Cape's Rake configuration API

--- lib/ninja_deploy.rb
+++ lib/ninja_deploy.rb
@@ -1,6 +1,20 @@
 require 'cape'
 
-module NinjaDeploy; end
+module NinjaDeploy
+
+  class << self
+    %w( local remote ).each do |location|
+      reader = "#{location}_rake_executable"
+      writer = "#{reader}="
+      [reader, writer].each do |method|
+        define_method method do |*arguments, &block|
+          Cape.send( method, *arguments, &block )
+        end
+      end
+    end
+  end
+
+end
 
 def NinjaDeploy( &block )
   Cape( &block )
--- spec/ninja_deploy_spec.rb
+++ spec/ninja_deploy_spec.rb
@@ -1,5 +1,21 @@
 require 'ninja_deploy'
 
+describe NinjaDeploy do
+  %w( local_rake_executable remote_rake_executable ).each do |attribute|
+    describe ".#{attribute}" do
+      it "should return the value of Cape.#{attribute}" do
+        Cape.stub!( attribute.to_sym ).and_return 'expected value'
+        NinjaDeploy.send( attribute ).should == 'expected value'
+      end
+
+      it "should set the value of Cape.#{attribute}" do
+        NinjaDeploy.send "#{attribute}=", 'passed-through value'
+        Cape.send( attribute ).should == 'passed-through value'
+      end
+    end
+  end
+end
+
 describe '#NinjaDeploy' do
   it 'should yield the Cape module if given a unary block' do
     yielded = nil
